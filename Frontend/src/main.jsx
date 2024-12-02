1f206b67a9ed1d4e24d08afd37800f8658040029
Yucheng-An <yuc.an2019@gmail.com>
2024-12-02 13:48:57 -0800

    Upload the frontend main.jsx file.

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../../../Downloads/final-yucheng-an-main/frontend/src/App.jsx'
+import '../../../Downloads/final-yucheng-an-main/frontend/src/assets/style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
