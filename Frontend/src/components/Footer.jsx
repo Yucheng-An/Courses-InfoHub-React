67bde72e6e637c143f14e2ac071dc277116ba540
Yucheng-An <yuc.an2019@gmail.com>
2024-12-04 13:31:57 -0800

    Upload React component Footer.jsx

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+      <footer className = "copyright">
+          <p>Copyright © 2024 - present. All rights reserved.</p>
+      </footer>
+  )
+}
+export default Footer;
